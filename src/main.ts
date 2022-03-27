e52ed568f8ac1675df045cd69694b6830f192083
bodoganabel <abel.bodogan@zooshgroup.com>
2022-03-28 01:55:27 +0200

    added env variables

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
 
+function showAppStage() {
+  console.log(
+    process.env.STAGE === 'production'
+      ? '🍏 STAGE:PRODUCTION 🍏'
+      : '🧱 STAGE:DEVELOPMENT 🧱',
+  );
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
 }
-bootstrap();
+
+async function main() {
+  showAppStage();
+  await bootstrap();
+  showAppStage();
+}
+
+main();
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,9 +4,14 @@ import { TasksService } from './tasks.service';
 import { TasksRepository } from './tasks.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TasksRepository]), AuthModule],
+  imports: [
+    TypeOrmModule.forFeature([TasksRepository]),
+    AuthModule,
+    ConfigModule,
+  ],
   controllers: [TasksController],
   providers: [TasksService],
 })
